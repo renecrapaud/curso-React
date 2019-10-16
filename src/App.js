9082dd0ac8026add5e38e9ce50b5f0540a3c7269
Rene <renecrapaud@gmail.com>
2019-10-16 12:56:11 +0000

    Recorrer listas

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import SeccionCondicional from './secciones/condicional'
 import './App.css';
 
 function App() {
+  const numeros = [1, 1, 3, 4, 5]
   return (
     <div className="App">
-        <SeccionCondicional />
+        <h4> Listas </h4>
+        {numeros.map((number, index) =>
+          {return <p key={index}> Soy el numero {number} </p> }
+        )}
     </div>
   );
 }
