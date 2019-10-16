0f7d6f930b0f0ccde43dd3496e1d54f598edc5ab
Rene <renecrapaud@gmail.com>
2019-10-16 11:33:02 +0000

    Condicional

--- src/secciones/condicional.js
+++ src/secciones/condicional.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react'
 
-class ComponenteA extends Component {
+class LoginButton extends Component {
   render () {
-    return <p>Componente A</p>
+    return <button>Iniciar sesión</button>
   }
 }
 
-class ComponenteB extends Component {
+class LogoutButton extends Component {
   render () {
-    return <p>Componente B</p>
+    return (
+      <div>
+        <p> Bienvenido usuario </p>
+        <button> Cerrar sesión </button>
+      </div>
+    )
   }
 }
 
-function muestraCondicional(muestraA){
+/* function muestraCondicional(muestraA){
   if(muestraA){
     return <ComponenteA />
   } else {
     return <ComponenteB />
   }
-}
+} */
 
 export default class SeccionCondicional extends Component {
   constructor () {
     super ()
-    this.state = {muestraA : false}
+    this.state = {isUserLogged : true}
   }
   render () {
     return <div>
         <h4>Renderizado condicional</h4>
-        {muestraCondicional(this.state.muestraA)}
+        {this.state.isUserLogged ? <LogoutButton /> : <LoginButton />}
       </div>
   }
 }
